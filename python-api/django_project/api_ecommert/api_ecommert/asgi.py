874e195483c12c24911c17a3a17d8e0b0429eb57
DungBT <dungbt@nals.vn>
2022-09-25 23:57:54 +0700

    init project

--- python-api/django_project/api_ecommert/api_ecommert/asgi.py
+++ python-api/django_project/api_ecommert/api_ecommert/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for django_project project.
+ASGI config for api_ecommert project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_ecommert.settings')
 
 application = get_asgi_application()
--- python-api/django_project/api_ecommert/manage.py
+++ python-api/django_project/api_ecommert/manage.py
@@ -3,8 +3,10 @@
 import os
 import sys
 
+
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_ecommert.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
